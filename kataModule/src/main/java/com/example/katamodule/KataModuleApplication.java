48e307cc7e21f6743567a6c5273a3b3b53870fee
danielmera2912 <92045080+danielmera2912@users.noreply.github.com>
2023-02-20 21:53:34 +0100

    modulo

---
+++ kataModule/src/main/java/com/example/katamodule/KataModuleApplication.java
@@ -0,0 +1,13 @@
+package com.example.katamodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KataModuleApplication.class, args);
+    }
+
+}
---
+++ kataModule/src/test/java/com/example/katamodule/KataModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.katamodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
